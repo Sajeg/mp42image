57b896dae67697b50da91919391b26f96f0d5d82
Sajeg <jfx@posteo.de>
2022-04-09 00:27:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import time
+
 import cv2
+from moviepy.video.io.VideoFileClip import *
 
 print('Welcome to the mp42image converter.')
 
@@ -9,6 +12,15 @@ while not file_path.__contains__(".mp4"):
     file_path = input("File Path: ")
 cam = cv2.VideoCapture(file_path)
 
+output = input("Output directory: ")
+while not os.path.exists(output):
+    print('Error no output directory does not exist. Please try again.')
+    output = input("Output directory: ")
+
+if not os.path.exists(output + '\OutputFrames'):
+    os.mkdir(output + '\OutputFrames')
+output = output + '\OutputFrames'
+
 change_fps = input('Change FPS (y/n): ')
 while not (change_fps == 'y' or change_fps == 'n'):
     print('Invalid answer. Please try again.')
@@ -16,13 +28,11 @@ while not (change_fps == 'y' or change_fps == 'n'):
 
 if change_fps == 'y':
     fps = input('FPS: ')
-    cam.set(cv2.CAP_PROP_FPS, int(fps))
+    clip = VideoFileClip(file_path)
+    clip.write_videofile(output + '\\video.mp4', fps=int(fps))
+    file_path = output + '\\video.mp4'
+    cam = cv2.VideoCapture(file_path)
     print('Successful changed FPS.')
-
-output = input("Output directory: ")
-if not os.path.exists(output + '\OutputFrames\ '):
-    os.mkdir(output + '\OutputFrames\ ')
-new_output = output + '\OutputFrames\Frame'
 
 frame_extension = input('Frame extensions (.png/.jpg): ')
 while not (frame_extension == '.png' or frame_extension == '.jpg'):
@@ -31,13 +41,14 @@ while not (frame_extension == '.png' or frame_extension == '.jpg'):
 
 # frame
 currentframe = 0
+output = output + '\Frame'
 
 while True:
 
     ret, frame = cam.read()
 
     if ret:
-        name = new_output + str(currentframe) + frame_extension
+        name = output + str(currentframe) + frame_extension
         print('Creating...' + name)
 
         cv2.imwrite(name, frame)
@@ -46,7 +57,10 @@ while True:
     else:
         break
 
-print('Successful extracted ' + currentframe + ' frames.')
+currentframe -= 1
+print('Successful extracted ' + str(currentframe) + ' frames.')
 
 cam.release()
 cv2.destroyAllWindows()
+
+time.sleep(3)
