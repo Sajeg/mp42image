cabe0c19ea2aaf65a79a16d8a34923260025d080
Sajeg <jfx@posteo.de>
2022-04-08 23:03:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-print('Welcome to the mp42jpg converter.')
+print('Welcome to the mp42image converter.')
 
 file_path = input("File Path: ")
 while not file_path.__contains__(".mp4"):
@@ -9,15 +9,25 @@ while not file_path.__contains__(".mp4"):
     file_path = input("File Path: ")
 cam = cv2.VideoCapture(file_path)
 
-if input('Change FPS (y/n): ') == 'y':
+change_fps = input('Change FPS (y/n): ')
+while not (change_fps == 'y' or change_fps == 'n'):
+    print('Invalid answer. Please try again.')
+    change_fps = input('Change FPS (y/n): ')
+
+if change_fps == 'y':
     fps = input('FPS: ')
     cam.set(cv2.CAP_PROP_FPS, int(fps))
     print('Successful changed FPS.')
 
 output = input("Output directory: ")
-if not os.path.exists(output + '\OutputFrames\Frame'):
+if not os.path.exists(output + '\OutputFrames\ '):
     os.mkdir(output + '\OutputFrames\ ')
 new_output = output + '\OutputFrames\Frame'
+
+frame_extension = input('Frame extensions (.png/.jpg): ')
+while not (frame_extension == '.png' or frame_extension == '.jpg'):
+    print('unknown file extension. Please try again.')
+    frame_extension = input('Frame extensions (.png/.jpg): ')
 
 # frame
 currentframe = 0
@@ -27,7 +37,7 @@ while True:
     ret, frame = cam.read()
 
     if ret:
-        name = new_output + str(currentframe) + '.png'
+        name = new_output + str(currentframe) + frame_extension
         print('Creating...' + name)
 
         cv2.imwrite(name, frame)
@@ -36,7 +46,7 @@ while True:
     else:
         break
 
-print('Successful extracted all frames.')
+print('Successful extracted ' + currentframe + ' frames.')
 
 cam.release()
 cv2.destroyAllWindows()
